ed4af92ffb82d59e3faf9f40aef51e994e952c59
miaomiao <237070612@qq.com>
2018-07-09 18:41:36 +0800

    change

--- view/invalid/invalid-per/invalid-demo/demo-img/js/validate.js
+++ view/invalid/invalid-per/invalid-demo/demo-img/js/validate.js
@@ -3,5 +3,5 @@ if ( validate !== '女王') {
     alert('这个页面没有任何东西，换一个页面看看吧');
     window.location.href="https://miaomiaolin.github.io/";
 } else {
-    loadImg();
+    document.getElementById("main").style.display = 'block';  
 }
