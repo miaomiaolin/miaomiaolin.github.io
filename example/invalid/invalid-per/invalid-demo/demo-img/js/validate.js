2f5f39e8193538391e568768a3522031567433e6
linmiaomiao <linmiaomiao@fanhantech.com>
2019-12-16 17:53:11 +0800

    静态资源上传

---
+++ example/invalid/invalid-per/invalid-demo/demo-img/js/validate.js
@@ -0,0 +1,7 @@
+var validate = prompt("你是？");
+if ( validate !== '女王') {
+    alert('这个页面没有任何东西，换一个页面看看吧');
+    window.location.href="https://miaomiaolin.github.io/";
+} else {
+    document.getElementById("main").style.display = 'block';  
+}
